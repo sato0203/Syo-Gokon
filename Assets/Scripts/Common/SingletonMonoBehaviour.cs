0a05f93044d5d68c69a9ad7277363cd0fd50a9d7
AkiSato <sato.geijo.13@gmail.com>
2017-09-16 12:19:13 +0900

    add InGameScene

---
+++ Assets/Scripts/Common/SingletonMonoBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+{
+	protected static T instance;
+	public static T Instance {
+		get {
+			if (instance == null) {
+				instance = (T)FindObjectOfType (typeof(T));
+				
+				if (instance == null) {
+					Debug.LogWarning (typeof(T) + " is nothing");
+				}
+			}
+			
+			return instance;
+		}
+	}
+	
+    protected virtual void Awake()
+	{
+		CheckInstance();
+	}
+	
+    protected virtual bool CheckInstance()
+	{
+		if( instance == null)
+		{
+			instance = (T)this;
+			return true;
+		}else if( Instance == this )
+		{
+			return true;
+		}
+ 
+		if (destroyGameObject)
+		{
+			Destroy (gameObject); 
+		}
+		else
+		{
+			Destroy(this);
+		}
+
+		return false;
+	}
+
+	protected virtual bool destroyGameObject{ get{ return false; } }
+}
